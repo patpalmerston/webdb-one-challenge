5af4171fd0fd0538835140c6850ace0785dc89a0
Pat Palmerston <patpalmerston@gmail.com>
2019-09-09 13:32:22 -0700

    get by id

--- data/accounts/accountsRouter.js
+++ data/accounts/accountsRouter.js
@@ -15,4 +15,17 @@ router.get('/', (req, res) => {
 		});
 });
 
+// get by id
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	db.getById(id)
+		.then(acc => {
+			res.status(200).json(acc);
+		})
+		.catch(err => {
+			console.log('Error', err);
+			res.status(500).json({ error: 'account could not be retrieved' });
+		});
+});
+
 module.exports = router;
