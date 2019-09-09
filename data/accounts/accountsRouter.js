744339a85b199749c3954546d363ba05611f12f3
Pat Palmerston <patpalmerston@gmail.com>
2019-09-09 13:10:55 -0700

    accountDb scaffolded for routes

---
+++ data/accounts/accountsRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const db = require('./data/dbConfig.js.js');
+const router = express.Router();
+
+// get
+router.get('/', (req, res) => {
+
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// const db = require('./data/dbConfig.js');
+const accountsRouter = require('./data/accounts/accountsRouter')
 
 const server = express();
 
 server.use(express.json());
+
+server.unsubscribe('/api/accounts', accountsRouter)
 
 server.use('/', (req, res) => {
 	res.send('<h1>Hello from Todays project</h1>');
